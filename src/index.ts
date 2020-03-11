921d8fbc9cf8c22bf80220469af309519f4221cf
Alan Rynne <alan@rynne.es>
2020-03-11 22:06:43 +0100

    Added Jest for testing and initial test

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,3 @@
-import fs from 'fs'
-import * as nearley from 'nearley'
 import ifcGrammar from './grammar/ifc' // Nearley generated file, not commited to source code!!
 
-
-var content = fs.readFileSync("examples/TestIFC-001.ifc", "utf8");
-
-const ifcParser = new nearley.Parser(nearley.Grammar.fromCompiled(ifcGrammar))
-
-ifcParser.feed(content)
-
-writeToPath(ifcParser.results, './results/ast.json');
-
-
-function writeToPath(jsObj: any, path: string) {
-    fs.writeFile(
-        path,
-        JSON.stringify(jsObj, null, 4),
-        (err) => {
-            // In case of a error throw err. 
-            if (err) throw err;
-        })
-}
+export default ifcGrammar;
---
+++ src/main.test.ts
@@ -0,0 +1,29 @@
+import fs from 'fs'
+import ifcGrammar from './index'
+import * as nearley from 'nearley'
+
+
+describe('Unambiguity test', function () {
+    it('TestIFC-001.ifc', () => mainIfcParserTest("examples/TestIFC-001.ifc", "./results/ast-testifc.json"))
+    it('Model_001.ifc', () => mainIfcParserTest("examples/Model_001.ifc", "./results/ast-testifc.json"))
+    it('Model_002.ifc', () => mainIfcParserTest("examples/Model_002.ifc", "./results/ast-testifc.json"))
+})
+
+
+function mainIfcParserTest(path: string, outPath: string) {
+    var content = fs.readFileSync(path, "utf8");
+    const ifcParser = new nearley.Parser(nearley.Grammar.fromCompiled(ifcGrammar))
+    ifcParser.feed(content)
+    expect(ifcParser.results.length).toBe(1);
+    writeToPath(ifcParser.results, outPath);
+}
+
+function writeToPath(jsObj: any, path: string) {
+    fs.writeFile(
+        path,
+        JSON.stringify(jsObj, null, 4),
+        (err) => {
+            // In case of a error throw err. 
+            if (err) throw err;
+        })
+}
