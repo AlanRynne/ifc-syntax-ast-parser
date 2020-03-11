74162e5094d6e934061a4c33a2fddf9ccfde9c1f
Alan Rynne <alan@rynne.es>
2020-03-11 22:44:40 +0100

    Added nearley types

--- src/main.test.ts
+++ src/main.test.ts
@@ -2,13 +2,11 @@ import fs from 'fs'
 import ifcGrammar from './index'
 import * as nearley from 'nearley'
 
-
 describe('Unambiguity test', function () {
     it('TestIFC-001.ifc', () => mainIfcParserTest("examples/TestIFC-001.ifc", "./results/ast-testifc.json"))
     it('Model_001.ifc', () => mainIfcParserTest("examples/Model_001.ifc", "./results/ast-testifc.json"))
     it('Model_002.ifc', () => mainIfcParserTest("examples/Model_002.ifc", "./results/ast-testifc.json"))
 })
-
 
 function mainIfcParserTest(path: string, outPath: string) {
     var content = fs.readFileSync(path, "utf8");
